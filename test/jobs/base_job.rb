9fb33901ab9b95e3e0a25476a22885ecc69a6ac9
Hermann Mayer <hermann.mayer92@gmail.com>
2022-03-16 08:53:51 +0100

    one of many [ci skip]
    Signed-off-by: Hermann Mayer <hermann.mayer92@gmail.com>

--- test/jobs/base_job.rb
+++ test/jobs/base_job.rb
@@ -14,6 +14,9 @@ class BaseJob < ActiveJob::Base
 
   # Pass in an identifier so we can distinguish different jobs
   def perform(id, sleep_time: 5)
+    # TODO: ArgumentError (wrong number of arguments (given 2, expected 1)):
+    # activejob-locking/test/jobs/base_job.rb:16:in `perform'
+    # activejob-locking/vendor/bundle/ruby/3.0.0/gems/activejob-5.2.6.2/lib/active_job/execution.rb:39:in `block in perform_now'
     sleep(sleep_time)
   end
 end
